6d3af5a756934032575e4291e65385899e05558e
ls059 <lyspers@gmail.com>
2018-02-27 08:20:04 -0500

    added bracket and main class

---
+++ src/com/company/bracketMaker.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by ls059 on 2/27/18.
+ */
+public class bracketMaker {
+}
---
+++ src/com/company/main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by ls059 on 2/27/18.
+ */
+public class main {
+
+    public static void main(String[] args){
+        System.out.println("");
+
+
+    }
+}
